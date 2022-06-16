df1acb4f6fa5e5ef5d5c61220b0062a0c3078831
sarah-14 <94878803+sarah-14@users.noreply.github.com>
2022-06-15 17:41:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+student = [];
+
+function submit()
+{
+    var name_1 = document.getElementById("name_of_the_student_1").value;
+ var name_2 = document.getElementById("name_of_the_student_2").value;
+var name_3 = document.getElementById("name_of_the_student_3").value;
+var name_4 = document.getElementById("name_of_the_student_4").value;
+
+
+student.push(name_1);
+student.push(name_2);
+student.push(name_3);
+student.push(name_4);
+
+console.log(student);
+
+document.getElementById("display_name").innerHTML = student;
+document.getElementById("submit_button").style.display = "none";
+document.getElementById("sort_button").style.display = "inline-block";
+
+}
+
+function sorting()
+{
+
+    student.sort();
+    console.log(student);
+    document.getElementById("display_name").innerHTML = student;
+    
+}
